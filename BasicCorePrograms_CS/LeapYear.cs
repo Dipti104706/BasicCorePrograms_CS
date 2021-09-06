40d8aa13f57e508365bfd36479a75bfef89e6c20
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-06 23:53:09 +0530

    [Dipti added code to find a leap year]

---
+++ BasicCorePrograms_CS/LeapYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicCorePrograms_CS
+{
+    class LeapYear
+    {
+        
+        public static void leap()
+        {
+            Console.WriteLine("Enter a year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ( ((year % 400) == 0)  ||  ((year % 4) == 0) && ((year % 100) != 0))
+            {
+                Console.WriteLine("{0} is a leap year", +year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap year", +year);
+            }
+        }
+        static void Main(string[] args)
+        {
+            LeapYear.leap();
+            Console.ReadLine();
+        }
+    }
+}
