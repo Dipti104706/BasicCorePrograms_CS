299ec9876fd01ecac1808b2c413fb039696e8c5f
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-07 21:25:02 +0530

    [Dipti] added code for swapping of two numbers

---
+++ BasicCorePrograms_CS/SwapNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BasicCorePrograms_CS
+{/// <summary>
+/// Template for swapping of two numbers
+/// </summary>
+    class SwapNumber
+    {
+        public static int temp;
+        //Program to swap two numbers
+        public static void Swapping()
+        {
+            //taking input from user
+            Console.WriteLine("Enter first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before swapping numbers are");
+            Console.WriteLine("num1=" + num1);
+            Console.WriteLine("num2=" + num2);
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("After swapping numbers are");
+            Console.WriteLine("num1=" +num1);
+            Console.WriteLine("num2=" +num2);
+        }
+
+        static void Main(string[] args)
+        {
+            SwapNumber.Swapping();
+            Console.ReadLine();
+        }
+    }
+}
