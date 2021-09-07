a7d1ca10ef71abfb1126ac6e5551af3694598ea8
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-07 17:52:45 +0530

    [Dipti] added code to find number is a even or odd

---
+++ BasicCorePrograms_CS/EvenOdd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BasicCorePrograms_CS
+{/// <summary>
+/// Template for even or odd number check
+/// </summary>
+    class EvenOdd
+    { 
+        //Program to check number is a even or odd
+        public static void NumberCheck()
+        {
+            //Take input from user
+            Console.WriteLine("Enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The given number is a even number");
+            }
+            else
+            {
+                Console.WriteLine("The given number is a odd number");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            EvenOdd.NumberCheck();
+            Console.ReadLine();
+        }
+    }
+}
